628059f9d3793e233335364b8be7596682519713
Mathy Vanhoef <Mathy.Vanhoef@kuleuven.be>
2020-07-24 12:07:58 +0400

    Add unload.sh script

---
+++ unload.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rmmod mac80211_hwsim
+rmmod mt76x2u mt76x2_common mt76x02_usb mt76_usb mt76x02_lib mt76
+rmmod carl9170 ath9k_htc ath9k_common ath9k_hw ath iwlmvm iwlwifi mac80211 cfg8021
